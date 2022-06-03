0f1f36fa8bcb6b2d5ec3484be2db2aca14718edc
Andy Doan <andy@foundries.io>
2022-06-03 14:13:34 -0500

    simulator: Add logic for supporting shared-volumes
    Signed-off-by: Andy Doan <andy@foundries.io>

--- runner/jobserv_runner/simulator.py
+++ runner/jobserv_runner/simulator.py
@@ -15,14 +15,57 @@ def main(args):
     m.handler.execute(args.worker_dir, args.runner_dir, args.rundef)
 
 
+def _update_shared_volumes_mapping(volumes, rundef):
+    """Convert rundef mappings:
+      name1: /path/in/container1
+      name2: /path/in/container2
+
+    And host config shared-volumes like:
+      name1: /path/on/host1
+      name2: /path/on/host2
+
+    to produce something we can mount with docker-run:
+      /path/on/host1: /path/in/container1
+      /path/on/host2: /path/in/container2
+    """
+    shared_vols = rundef.get("shared-volumes")
+    if shared_vols:
+        mapping = {}
+        for name, container_path in shared_vols.items():
+            try:
+                host_path = volumes[name]
+                mapping[host_path] = container_path
+            except KeyError:
+                sys.exit(f"Please specify a shared volume path for: {name}")
+        rundef["shared-volumes"] = mapping
+
+
 def get_args(args=None):
     parser = argparse.ArgumentParser(description="Execute a JobServ run definition")
     parser.add_argument("-w", "--worker-dir", help="Location to store the run")
+    parser.add_argument(
+        "-v",
+        "--shared-volume",
+        action="append",
+        help="""Add a shared-volume mapping for run. Can be repeated.
+                Example: -v foo=/tmp/foo""",
+    )
     parser.add_argument("rundef", type=argparse.FileType("r"))
     args = parser.parse_args()
 
+    vols = {}
+    for v in args.shared_volume or []:
+        parts = v.split("=")
+        if len(parts) != 2:
+            sys.exit(f"Invalid shared-volume: {v}")
+        k, v = parts
+        if not os.path.isdir(v):
+            sys.exit(f"Invalid shared-volume: {k}. {v} does not exist")
+        vols[k] = v
+
     args.rundef = json.load(args.rundef)
     args.rundef["simulator"] = True
+    _update_shared_volumes_mapping(vols, args.rundef)
 
     if not os.path.isdir(args.worker_dir):
         sys.exit("worker-dir does not exist: " + args.worker_dir)
